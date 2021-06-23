05dc013fee94576a823fa3d83dda2ca04d9a6a2c
mauricioolarte <reneolarte@yahoo.com>
2021-06-22 23:39:40 -0500

    13-bind_user.js

--- 0x0C-Javascript_advanced/13-bind_user.js
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -1,1 +1,16 @@
-console.log(...in process)
+const user = {
+	hobby: "Calligraphy",
+	favoriteSport: "Hockey",
+	astrologicalSign: "Aries",
+	firstName: "Buillaume",
+	lastName: "Ialva",
+	location: "Telaviv",
+	occupation: "Engineer"
+}
+
+function logWelcomeUser(welcomeString) {
+	console.log(`${welcomeString}, ${this.firstName}. Your occupation is: ${this.occupation}`)
+}
+
+const bindLogWelcomeUser = logWelcomeUser.bind(user);
+bindLogWelcomeUser('hello')
