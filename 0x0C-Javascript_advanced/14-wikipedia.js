11d4f29cf692cc9b02dce13bd6e25c4fae83eedd
mauricioolarte <reneolarte@yahoo.com>
2021-06-23 00:18:22 -0500

    14-wikipedia.js

--- 0x0C-Javascript_advanced/14-wikipedia.js
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -1,1 +1,28 @@
-console.log(...in process)
+function createElement(data) {
+	let p = document.createElement("p");
+	p.textContent = data;
+	document.body.appendChild(p)
+}
+
+function queryWikipedia(callback) {
+	req = new XMLHttpRequest();
+	req.open("GET", "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*")
+	req.onreadystatechange = function () {
+		// In local files, status is 0 upon success in Mozilla Firefox
+		if (req.readyState === XMLHttpRequest.DONE) {
+			var status = req.status;
+			if (status === 0 || (status >= 200 && status < 400)) {
+				// The request has been completed successfully
+				// console.log(req.responseText);
+				callback(req.responseText);
+			} else {
+				console.log("error")
+			}
+		}
+	};
+	req.send();
+
+
+}
+
+queryWikipedia(createElement);
